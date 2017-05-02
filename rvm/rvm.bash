de937600c479e19dfea1d6a59f502edf5781f8a3
Michael Kelly <michaelkelly322@gmail.com>
2017-05-02 04:39:13 -0400

    :bug: Fix warnings from rvm about PATH var

--- rvm/rvm.bash
+++ rvm/rvm.bash
@@ -1,4 +1,4 @@
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 if [ -f ~/.rvm/scripts/rvm ]; then
   source ~/.rvm/scripts/rvm
 fi
