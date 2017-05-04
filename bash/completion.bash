5ebdfc57d64565f80979abb517b5a214015dde22
Michael Kelly <michaelkelly322@gmail.com>
2017-05-04 02:23:24 -0400

    :bug: Fix git-promote in non-interactive shells

--- bash/completion.bash
+++ bash/completion.bash
@@ -1,6 +1,13 @@
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
 if ! [ -n "$BASH_VERSION" ];then
   if ! shopt -oq posix; then
     if [ -f /usr/share/bash-completion/bash_completion ]; then
