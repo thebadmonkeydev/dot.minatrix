1bb28a26fc33fe52c0e54fce80fa04663e85f255
Michael Kelly <michaelkelly322@gmail.com>
2017-05-09 03:48:19 -0400

    :chore: expose script helpers to the BASH environment

---
+++ bash/helpers.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# A set of helpers for basic scripting and Bash executables
+source $DOTFILES/script/helpers
+
+# probably want to split this into:
+#
+#  1. io-helpers
+#  2. scripting-helpers
+#  3. dev-helpers
+#  4. etc. and place them in bash/ with .bash extension
